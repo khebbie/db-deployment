fa310c94a84ae429b9fb24a21f9e5dc21be03a13
Klaus Hebsgaard <klaus@hebsgaard.dk>
2013-01-21 21:06:30 +0100

    comment

--- testRun.ps1
+++ testRun.ps1
@@ -1,1 +1,2 @@
-﻿.\dbDeployment.ps1 "Data Source=wks-klh-1;Initial Catalog=ce-shop-drift;User ID=sa;Password=ExpandIT1;Packet Size=512"
+﻿#you should add your own local connectionstring to test this.
+.\dbDeployment.ps1 "Data Source=wks-klh-1;Initial Catalog=ce-shop-drift;User ID=sa;Password=ExpandIT1;Packet Size=512"
