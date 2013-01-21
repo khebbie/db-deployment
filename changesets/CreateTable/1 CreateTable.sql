abf072e932f898b1ce8eebf782fb589238ba9795
Klaus Hebsgaard <klaus@hebsgaard.dk>
2013-01-21 21:09:33 +0100

    no need for my initials to exist in a example file

--- changesets/CreateTable/1 CreateTable.sql
+++ changesets/CreateTable/1 CreateTable.sql
@@ -1,5 +1,5 @@
-if not exists (select * from sysobjects where name='db_changesklh' and xtype='U')
-    create table db_changesklh (
+if not exists (select * from sysobjects where name='db_changestmp' and xtype='U')
+    create table db_changestmp (
         ChangeSet varchar(255) not null,
 		ChangeDate DateTime not null
     )
